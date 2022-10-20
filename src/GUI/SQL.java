8d2d03990bc35d938eb213fadcb777dbfbacc936
bxy0128 <1326551161@qq.com>
2022-10-20 11:31:42 +0800

    1

---
+++ src/GUI/SQL.java
@@ -0,0 +1,49 @@
+package GUI;
+
+import java.sql.*;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+public class SQL {
+
+public  Statement sql;
+
+
+     {
+            try{
+          Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver"); //加载数据库驱动
+
+     }
+        catch(ClassNotFoundException ex){
+          System.out.println(ex);
+     }
+          try{
+               Connection con;
+
+               ResultSet rs;
+               String url,userName,userPwd;
+               // 连接数据库的语句
+               url="jdbc:sqlserver://localhost:1433;DatabaseName=Hospital";
+
+               userName="sa";
+               userPwd="123456";
+               con=DriverManager.getConnection(url,userName,userPwd);
+               sql=con.createStatement();
+               System.out.println("数据库连接成功");
+     }
+          catch(SQLException exp){
+               System.out.println(exp);
+               System.out.println("数据库连接失败");
+          }
+
+
+
+
+}
+
+
+
+}
+
+
---
+++ src/GUI/main.java
@@ -0,0 +1,12 @@
+package GUI;
+
+
+
+public class main {
+    public static void main(String[] args) {
+
+        new loginFrame();
+
+
+    }
+}
